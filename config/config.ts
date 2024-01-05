afc4ca9929ef7d2d8678d95043bcf06332370f35
xuyong04 <xuyong04@baidu.com>
2024-01-05 17:32:55 +0800

    博客

--- config/config.ts
+++ config/config.ts
@@ -5,5 +5,12 @@ const { REACT_APP_ENV } = process.env;
 export default defineConfig({
     routes: routes,
     npmClient: 'npm',
-    proxy: proxy['dev']
+    proxy: proxy['dev'],
+    favicons: [
+        // 完整地址
+        'https://zh.wikipedia.org/static/favicon/wikipedia.ico',
+        // 此时将指向 `/favicon.png` ，确保你的项目含有 `public/favicon.png`
+        //'/favicon.png'
+    ]
+
 });
