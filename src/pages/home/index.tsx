2dadfb0d807fcfbfdefadefec967ed46ce33a0aa
xuyong04 <xuyong04@baidu.com>
2024-02-25 11:17:16 +0800

    博客

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -69,7 +69,7 @@ export default function HomePage() {
                     <Header display={display} />
                     <BackTop className="back-top"/>
                     <h2 style={{marginTop: 60}}>{resp.article?.title}</h2>
-                    <span className="tag"><Tag style={{color: "#999"}} color="rgba(0,0,0,.05)">原创</Tag><a className="author">栗·Leseclaireurs</a> <span> 2023-12-05 19:34 发表于北京 </span></span>
+                    <span className="tag"><Tag style={{color: "#999", zIndex:-1}} color="rgba(0,0,0,.05)">原创</Tag><a className="author">栗·Leseclaireurs</a> <span> 2023-12-05 19:34 发表于北京 </span></span>
                     {!loading && <Viewer ref={editor_ref} initialValue={resp.article?.content} plugins={[
                         [codeSyntaxHighlightPlugin, {highlighter: Prism}],
                         umlPlugin,
